c518618b00c3803c3f4c11d3a281f354a14819cf
Gifhary Syidhqa Hamim <gifhary.1997@gmail.com>
2020-11-11 14:20:01 +0700

    added some shits

---
+++ lib/screen/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
